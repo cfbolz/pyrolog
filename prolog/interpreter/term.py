08524efbb901aa5759f8cc64b2c4d0e62af666b7
CF Bolz-Tereick <cfbolz@gmx.de>
2010-04-19 23:57:47 +0200

    whitespace

--- prolog/interpreter/term.py
+++ prolog/interpreter/term.py
@@ -18,39 +18,39 @@ def debug_print(*args):
 class PrologObject(object):
     __slots__ = ()
     _immutable_ = True
-    
+
     def getvalue(self, heap):
         return self
-    
+
     def dereference(self, heap):
         raise NotImplementedError("abstract base class")
-    
+
     def copy(self, heap, memo):
         raise NotImplementedError("abstract base class")
-    
+
     def copy_standardize_apart(self, heap, env):
         raise NotImplementedError("abstract base class")
-    
+
     def unify_and_standardize_apart(self, other, heap, env):
         raise NotImplementedError("abstract base class")
-    
+
     def enumerate_vars(self, memo):
         raise NotImplementedError("abstract base class")
-    
+
     @specialize.arg(3)
     def unify(self, other, heap, occurs_check=False):
         raise NotImplementedError("abstract base class")
-    
+
     @specialize.arg(3)
     def _unify_derefed(self, other, heap, occurs_check=False):
         raise NotImplementedError("abstract base class")
-    
+
     def contains_var(self, var, heap):
         return False
-    
+
     def eval_arithmetic(self, engine):
         error.throw_type_error("evaluable", self)
-    
+
     def cmp_standard_order(self, other, heap):
         raise NotImplementedError("abstract base class")
 
@@ -59,12 +59,12 @@ class PrologObject(object):
 
 class Var(PrologObject):
     TYPE_STANDARD_ORDER = 0
-    
+
     created_after_choice_point = None
-    
+
     def __init__(self):
         self.binding = None
-    
+
     @specialize.arg(3)
     @jit.unroll_safe
     def unify(self, other, heap, occurs_check=False):
@@ -79,7 +79,7 @@ class Var(PrologObject):
         else:
             assert isinstance(next, NonVar)
             return next._unify_derefed(other, heap, occurs_check)
-    
+
     @specialize.arg(3)
     def _unify_derefed(self, other, heap, occurs_check=False):
         if isinstance(other, Var) and other is self:
@@ -88,7 +88,7 @@ class Var(PrologObject):
             raise UnificationFailed()
         else:
             self.setvalue(other, heap)
-    
+
     def dereference(self, heap):
         next = self.binding
         if next is None:
@@ -99,17 +99,17 @@ class Var(PrologObject):
                 # do path compression
                 self.setvalue(result, heap)
             return result
-    
+
     def getvalue(self, heap):
         res = self.dereference(heap)
         if not isinstance(res, Var):
             return res.getvalue(heap)
         return res
-    
+
     def setvalue(self, value, heap):
         heap.add_trail(self)
         self.binding = value
-    
+
     def copy(self, heap, memo):
         self = self.dereference(heap)
         if isinstance(self, Var):
@@ -120,10 +120,10 @@ class Var(PrologObject):
             memo.set(self, newvar)
             return newvar
         return self.copy(heap, memo)
-    
+
     def enumerate_vars(self, memo):
         return memo.get(self)
-    
+
     def contains_var(self, var, heap):
         self = self.dereference(heap)
         if self is var:
@@ -131,23 +131,23 @@ class Var(PrologObject):
         if not isinstance(self, Var):
             return self.contains_var(var, heap)
         return False
-    
+
     def __repr__(self):
         return "Var(%s)" % (self.binding, )
 
-    
+
     def __eq__(self, other):
         # for testing
         # XXX delete
         return self is other
-    
+
     def eval_arithmetic(self, engine):
         self = self.dereference(engine.heap)
         if isinstance(self, Var):
             error.throw_instantiation_error()
-        
+
         return self.eval_arithmetic(engine)
-    
+
     @jit.dont_look_inside
     def cmp_standard_order(self, other, heap):
         assert isinstance(other, Var)
@@ -159,7 +159,7 @@ class NumberedVar(PrologObject):
     _immutable_ = True
     def __init__(self, index):
         self.num = index
-    
+
     def copy_standardize_apart(self, heap, env):
         if self.num < 0:
             return heap.newvar()
@@ -167,7 +167,7 @@ class NumberedVar(PrologObject):
         if res is None:
             res = env[self.num] = heap.newvar()
         return res
-    
+
     def unify_and_standardize_apart(self, other, heap, env):
         if self.num < 0:
             return other
@@ -177,44 +177,44 @@ class NumberedVar(PrologObject):
             return other
         res.unify(other, heap)
         return res
-    
+
     def dereference(self, heap):
         return self
-    
+
     def __repr__(self):
         return "NumberedVar(%s)" % (self.num, )
 
 
 class NonVar(PrologObject):
     __slots__ = ()
-    
+
     def dereference(self, heap):
         return self
-    
+
     # needs to be overridden in non-atomic subclasses
     def copy(self, heap, memo):
         return self
-    
+
     # needs to be overridden in non-atomic subclasses
     def copy_standardize_apart(self, heap, memo):
         return self
-    
+
     @specialize.arg(3)
     def unify(self, other, heap, occurs_check=False):
         other = other.dereference(heap)
         return self._unify_derefed(other, heap, occurs_check)
-    
+
     @specialize.arg(3)
     def basic_unify(self, other, heap, occurs_check=False):
         raise NotImplementedError("abstract base class")
-    
+
     @specialize.arg(3)
     def _unify_derefed(self, other, heap, occurs_check=False):
         if isinstance(other, Var):
             other._unify_derefed(self, heap, occurs_check)
         else:
             self.basic_unify(other, heap, occurs_check)
-    
+
     def unify_and_standardize_apart(self, other, heap, env):
         other = other.dereference(heap)
         if isinstance(other, Var):
@@ -223,35 +223,35 @@ class NonVar(PrologObject):
             return copy
         else:
             return self.copy_and_basic_unify(other, heap, env)
-    
+
     def copy_and_basic_unify(self, other, heap, env):
         raise NotImplementedError("abstract base class")
-    
+
     def enumerate_vars(self, memo):
         return self
 
 class Callable(NonVar):
     _immutable_ = True
     __slots__ = ()
-    
+
     def name(self):
         return self.signature().name
-        
+
     def signature(self):
         raise NotImplementedError("abstract base")
-    
+
     def get_prolog_signature(self):
         return Callable.build("/", [Callable.build(self.name()),
                                     Number(self.argument_count())])
     def arguments(self):
         raise NotImplementedError("abstract base")
-    
+
     def argument_at(self, i):
         raise NotImplementedError("abstract base")
-    
+
     def argument_count(self):
         raise NotImplementedError("abstract base")
-    
+
     @specialize.arg(3)
     def basic_unify(self, other, heap, occurs_check=False):
         if (isinstance(other, Callable) and
@@ -260,7 +260,7 @@ class Callable(NonVar):
                 self.argument_at(i).unify(other.argument_at(i), heap, occurs_check)
         else:
             raise UnificationFailed
-    
+
     def copy_and_basic_unify(self, other, heap, env):
         if (isinstance(other, Callable) and
             self.signature().eq(other.signature())):
@@ -270,19 +270,19 @@ class Callable(NonVar):
                 argself.unify_and_standardize_apart(argother, heap, env)
         else:
             raise UnificationFailed
-    
+
     def copy(self, heap, memo):
         return self._copy_term(_term_copy, heap, memo)
-    
+
     def copy_standardize_apart(self, heap, env):
         return self._copy_term(_term_copy_standardize_apart, heap, env)
-    
+
     def enumerate_vars(self, memo):
         return self._copy_term(_term_enumerate_vars, None, memo)
-    
+
     def getvalue(self, heap):
         return self._copy_term(_term_getvalue, heap)
-    
+
     @specialize.arg(1)
     @jit.unroll_safe
     def _copy_term(self, copy_individual, heap, *extraargs):
@@ -300,13 +300,13 @@ class Callable(NonVar):
             return Callable.build(self.name(), args, self.signature(), heap=heap)
         else:
             return self
-    
+
     def contains_var(self, var, heap):
         for arg in self.arguments():
             if arg.contains_var(var, heap):
                 return True
         return False
-    
+
     def cmp_standard_order(self, other, heap):
         assert isinstance(other, Callable)
         c = rcmp(self.argument_count(), other.argument_count())
@@ -322,15 +322,15 @@ class Callable(NonVar):
             if c != 0:
                 return c
         return 0
-    
+
     def eval_arithmetic(self, engine):
         from prolog.interpreter.arithmetic import get_arithmetic_function
-        
+
         func = get_arithmetic_function(self.signature())
         if func is None:
             error.throw_type_error("evaluable", self.get_prolog_signature())
         return func(engine, self)
-    
+
     @staticmethod
     @jit.unroll_safe
     def build(term_name, args=None, signature=None, heap=None, cache=True):
@@ -364,7 +364,7 @@ class Callable(NonVar):
             if cls is not None:
                 return cls(term_name, args, signature)
             return Term(term_name, args, signature)
-    
+
     @staticmethod
     @jit.purefunction
     def _find_specialized_class(term_name, numargs):
@@ -406,18 +406,18 @@ class Atom(Callable):
     __slots__ = ('_name', '_signature')
     cache = {}
     _immutable_ = True
-    
+
     def __init__(self, name, signature=None):
         if signature is None:
             signature = Signature(name, 0)
         self._signature = signature
-    
+
     def __str__(self):
         return self.name()
-    
+
     def __repr__(self):
         return "Atom(%r)" % (self.name(),)
-    
+
     @staticmethod
     def newatom(name):
         result = Atom.cache.get(name, None)
@@ -426,7 +426,7 @@ class Atom(Callable):
         signature = Signature.getsignature(name, 0)
         Atom.cache[name] = result = Atom(name, signature)
         return result
-    
+
     def eval_arithmetic(self, engine):
         #XXX beautify that
         if self.name() == "pi":
@@ -434,19 +434,19 @@ class Atom(Callable):
         if self.name() == "e":
             return Float.e
         error.throw_type_error("evaluable", self.get_prolog_signature())
-    
+
     def arguments(self):
         return []
-    
+
     def argument_at(self, i):
         raise IndexError
-    
+
     def argument_count(self):
         return 0
-    
+
     def name(self):
         return self._signature.name
-    
+
     def signature(self):
         return self._signature
 
@@ -454,31 +454,31 @@ class Number(NonVar): #, UnboxedValue):
     TYPE_STANDARD_ORDER = 2
     _immutable_ = True
     __slots__ = ("num", )
-    
+
     def __init__(self, num):
         self.num = num
-    
+
     @specialize.arg(3)
     def basic_unify(self, other, heap, occurs_check=False):
         if isinstance(other, Number) and other.num == self.num:
             return
         raise UnificationFailed
-    
+
     def copy_and_basic_unify(self, other, heap, env):
         if isinstance(other, Number) and other.num == self.num:
             return self
         else:
             raise UnificationFailed
-    
+
     def __str__(self):
         return repr(self.num)
-    
+
     def __repr__(self):
         return "Number(%r)" % (self.num, )
-    
+
     def eval_arithmetic(self, engine):
         return self
-    
+
     def cmp_standard_order(self, other, heap):
         # XXX looks a bit terrible
         if isinstance(other, Number):
@@ -498,29 +498,29 @@ class Float(NonVar):
     _immutable_ = True
     def __init__(self, floatval):
         self.floatval = floatval
-    
+
     @specialize.arg(3)
     def basic_unify(self, other, heap, occurs_check=False):
         if isinstance(other, Float) and other.floatval == self.floatval:
             return
         raise UnificationFailed
-    
+
     def copy_and_basic_unify(self, other, heap, env):
         if isinstance(other, Float) and other.floatval == self.floatval:
             return self
         else:
             raise UnificationFailed
-    
+
     def __str__(self):
         return repr(self.floatval)
-    
+
     def __repr__(self):
         return "Float(%r)" % (self.floatval, )
-    
+
     def eval_arithmetic(self, engine):
         from prolog.interpreter.arithmetic import norm_float
         return norm_float(self)
-    
+
     def cmp_standard_order(self, other, heap):
         # XXX looks a bit terrible
         if isinstance(other, Number):
@@ -555,27 +555,27 @@ class Term(Callable):
     _immutable_ = True
     _immutable_fields_ = ["_args[*]"]
     __slots__ = ('_name', '_signature', '_args')
-    
+
     def __init__(self, term_name, args, signature):
         assert signature.name == term_name
         self._args = make_sure_not_resized(args)
         self._signature = signature
-    
+
     def __repr__(self):
         return "Term(%r, %r)" % (self.name(), self.arguments())
-    
+
     def __str__(self):
         return "%s(%s)" % (self.name(), ", ".join([str(a) for a in self.arguments()]))
-    
+
     def arguments(self):
         return self._args
-    
+
     def argument_at(self, i):
         return self._args[i]
-    
+
     def argument_count(self):
         return len(self._args)
-    
+
     def signature(self):
         return self._signature
 
@@ -599,28 +599,28 @@ def generate_class(cname, fname, n_args):
     parent = callables['Abstract', n_args]
     assert parent is not None
     signature = Signature.getsignature(fname, n_args)
-    
+
     class cls(parent):
         _immutable_ = True
         if n_args == 0:
             TYPE_STANDARD_ORDER = Atom.TYPE_STANDARD_ORDER
         else:
             TYPE_STANDARD_ORDER = Term.TYPE_STANDARD_ORDER
-        
+
         def __init__(self, term_name, args, signature):
             parent._init_values(self, args)
             assert self.name() == term_name
             assert args is None or len(args) == n_args
-                
+
         def name(self):
             return fname
-        
+
         def signature(self):
             return signature
 
         def _make_new(self, name, signature):
             return cls(name, None, signature)
-    
+
     cls.__name__ = cname
     return cls
 
@@ -637,19 +637,19 @@ def generate_abstract_class(n_args):
 
         def _make_new(self, name, signature):
             raise NotImplementedError("abstract base class")
-        
+
         def arguments(self):
             result = [None] * n_args
             for x in arg_iter:
                 result[x] = getattr(self, 'val_%d' % x)
             return result
-        
+
         def argument_at(self, i):
             for x in arg_iter:
                 if x == i:
                     return getattr(self, 'val_%d' % x)
             raise IndexError
-        
+
         def argument_count(self):
             return n_args
 
@@ -710,18 +710,18 @@ def generate_abstract_class(n_args):
                 return result
             else:
                 return self
-    
+
     abstract_callable.__name__ = 'Abstract'+str(n_args)
     return abstract_callable
 
 def generate_generic_class(n_args):
     parent = callables['Abstract', n_args]
     assert parent is not None
-    
+
     class generic_callable(parent):
         _immutable_ = True
         TYPE_STANDARD_ORDER = Term.TYPE_STANDARD_ORDER
-        
+
         def __init__(self, term_name, args, signature):
             parent._init_values(self, args)
             self._signature = signature
@@ -730,13 +730,13 @@ def generate_generic_class(n_args):
 
         def _make_new(self, name, signature):
             return generic_callable(name, None, signature)
-        
+
         def signature(self):
             return self._signature
-        
+
     generic_callable.__name__ = 'Generic'+str(n_args)
     return generic_callable
-    
+
 
 specialized_term_classes = {}
 callables = {}
