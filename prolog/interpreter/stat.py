cc4d663d1b67e275d9a4664c3138da36fa89e302
CF Bolz-Tereick <cfbolz@gmx.de>
2010-05-16 23:35:59 +0200

    don't need to store n

--- prolog/interpreter/stat.py
+++ prolog/interpreter/stat.py
@@ -1,7 +1,6 @@
 
 class LRUHistogram(object):
     def __init__(self, n=5):
-        self.n = n
         self.counts = [0] * n
         self.objects = [None] * n
         self.usage = [n - 1] * n
@@ -11,11 +10,11 @@ class LRUHistogram(object):
             i = self.objects.index(obj)
         except ValueError:
             # use either an empty place, or evict an object
-            i = self.usage.index(self.n - 1)
+            i = self.usage.index(self.n() - 1)
             self.objects[i] = obj
         self.counts[i] += 1
         oldusage = self.usage[i]
-        for j in range(self.n):
+        for j in range(self.n()):
             if -1 < self.usage[j] < oldusage:
                 self.usage[j] += 1
         self.usage[i] = 0
@@ -23,3 +22,6 @@ class LRUHistogram(object):
 
     def get_usage(self, obj):
         return self.counts[self.objects.index(obj)]
+
+    def n(self):
+        return len(self.counts)
