dd715a6f4ae7ab9a66b1f4b025a1d392015cf4f5
CF Bolz-Tereick <cfbolz@gmx.de>
2011-05-22 18:15:35 +0200

    fish out the files shape.py and test_shape.py from the compress-terms branch (which is stale)

---
+++ prolog/interpreter/shape.py
@@ -0,0 +1,132 @@
+from prolog.interpreter.term import Callable
+# a Callable implementation that tries to save memory
+
+class Shape(object):
+    def __init__(self):
+        pass
+
+    def resolve(self, storage):
+        raise NotImplementedError("abstract base class")
+
+    def resolve_at(self, argnum, storage):
+        w_obj = self.resolve(storage)
+        if isinstance(w_obj, Callable):
+            return w_obj.argument_at(argnum)
+        raise TypeError
+
+    def _compute_new_shape(self, memo):
+        raise NotImplementedError("abstract base class")
+
+class WrapShape(Shape):
+    def __init__(self, w_obj):
+        Shape.__init__(self)
+        self.w_obj = w_obj
+
+    def resolve(self, storage):
+        return self.w_obj
+
+    def _compute_new_shape(self, memo):
+        return self
+
+class InStorageShape(Shape):
+    def __init__(self, num):
+        Shape.__init__(self)
+        self.num = num
+
+    def resolve(self, storage):
+        return storage[self.num]
+
+    def _compute_new_shape(self, memo):
+        num = memo.setdefault(self.num, len(memo))
+        if num == self.num:
+            return self
+        return InStorageShape(num)
+
+class SharingShape(Shape):
+    def __init__(self, signature, children, needs_reshaping=True):
+        Shape.__init__(self)
+        self.signature = signature
+        self.children = children
+        if needs_reshaping:
+            self.reshaper = make_reshaper(self)
+        else:
+            self.reshaper = None
+
+    def resolve(self, storage):
+        if self.reshaper is not None:
+            return self.reshaper.reshape(storage)
+        else:
+            return ShapedCallable(self, storage)
+
+    def resolve_at(self, i, storage):
+        return self.children[i].resolve(storage)
+
+    @staticmethod
+    def build_potentially_wrap(signature, children):
+        unwrapped = [None] * len(children)
+        for i in range(len(children)):
+            child = children[i]
+            if not isinstance(child, WrapShape):
+                return SharingShape(signature, children)
+            unwrapped[i] = child.w_obj
+        return WrapShape(Callable.build(signature.name, unwrapped,
+                                        signature=signature))
+
+    def _compute_new_shape(self, memo):
+        children = [None] * len(self.children)
+        for i in range(len(self.children)):
+            child = self.children[i]._compute_new_shape(memo)
+            children[i] = child
+        return SharingShape(self.signature, children, False)
+
+
+def make_reshaper(shape):
+    memo = {}
+    newshape = shape._compute_new_shape(memo)
+    storage_shaper = [-1] * len(memo)
+    for key, value in memo.iteritems():
+        storage_shaper[value] = key
+    return Reshaper(storage_shaper, newshape)
+
+class Reshaper(object):
+    def __init__(self, storage_shaper, newshape):
+        assert newshape.reshaper is None
+        self.newshape = newshape
+        self.storage_shaper = storage_shaper
+
+    def reshape(self, storage):
+        newstorage = [None] * len(self.storage_shaper)
+        for i in range(len(self.storage_shaper)):
+            newstorage[i] = storage[self.storage_shaper[i]]
+        return ShapedCallable(self.newshape, newstorage)
+
+
+# _____________________________________________________________________
+
+class ShapedCallable(Callable):
+    def __init__(self, shape, storage):
+        assert isinstance(shape, SharingShape)
+        self.shape = shape
+        self.storage = storage
+
+    def signature(self):
+        return self.shape.signature
+
+    def argument_at(self, i):
+        return self.shape.resolve_at(i, self.storage)
+
+    def argument_count(self):
+        return self.shape.signature.numargs
+
+# _____________________________________________________________________
+
+def term_with_numbered_vars_to_shape(w_obj):
+    from prolog.interpreter import term
+    if isinstance(w_obj, term.NumberedVar):
+        return InStorageShape(w_obj.num)
+    elif isinstance(w_obj, Callable):
+        argshapes = [term_with_numbered_vars_to_shape(w_arg)
+                        for w_arg in w_obj.arguments()]
+        return SharingShape.build_potentially_wrap(w_obj.signature(), argshapes)
+    return WrapShape(w_obj)
+
---
+++ prolog/interpreter/test/test_shape.py
@@ -0,0 +1,99 @@
+from prolog.interpreter import shape, term, signature
+
+def test_wrap():
+    s = shape.WrapShape(term.Callable.build("a", [term.Number(1)]))
+    w_obj = s.resolve([1, 2, 3])
+    assert w_obj.name() == "a"
+    assert w_obj.argument_at(0).num == 1
+
+    assert s.resolve_at(0, None).num == 1
+
+def test_instorage():
+    s = shape.InStorageShape(0)
+    assert s.resolve([1, 2, 3]) == 1
+
+    w_obj = s.resolve_at(0, [term.Callable.build("a", [term.Number(1)])])
+    assert w_obj.num == 1
+
+def test_sharing():
+    sig = signature.Signature.getsignature("f", 2)
+    for b in [True, False]:
+        s = shape.SharingShape(sig, [
+            shape.WrapShape(term.Callable.build("a")),
+            shape.InStorageShape(1)
+        ], needs_reshaping=b)
+        assert s.resolve_at(0, [1, 2]).name() == "a"
+        assert s.resolve_at(1, [1, 2]) == 2
+
+    w_obj = s.resolve([1, 2])
+    assert isinstance(w_obj, shape.ShapedCallable)
+    w_obj.argument_at(0).name() == "a"
+    w_obj.argument_at(1) == 2
+
+def test_build_potentially_wrap():
+    sig = signature.Signature.getsignature("f", 2)
+    sh = shape.SharingShape.build_potentially_wrap(
+        sig, [shape.WrapShape(term.Number(1)),
+              shape.WrapShape(term.Callable.build("a"))])
+    assert isinstance(sh, shape.WrapShape)
+
+def test_term_with_numbered_vars_to_shape():
+    w_obj = term.Callable.build("f", [term.Callable.build("a"),
+                                      term.Number(12),
+                                      term.NumberedVar(0),
+                                      term.NumberedVar(1)])
+    s = shape.term_with_numbered_vars_to_shape(w_obj)
+    assert isinstance(s, shape.SharingShape)
+    assert s.children[0].w_obj.signature().name == "a"
+    assert s.children[1].w_obj.num == 12
+    assert s.children[2].num == 0
+    assert s.children[3].num == 1
+
+
+    w_obj = term.Callable.build("f", [term.Callable.build("a"),
+                                      term.Number(12)])
+    s = shape.term_with_numbered_vars_to_shape(w_obj)
+    assert isinstance(s, shape.WrapShape)
+    assert s.w_obj.signature().name == "f"
+
+def test_reshape():
+    sig = signature.Signature.getsignature("f", 3)
+    s = shape.SharingShape(sig, [
+        shape.WrapShape(term.Callable.build("a")),
+        shape.InStorageShape(0),
+        shape.InStorageShape(1)
+    ], False)
+    rs = shape.Reshaper([5, 2], s)
+    w_obj = rs.reshape(["a", "b", "c", "d", "e", "f"])
+    assert w_obj.argument_at(1) == "f"
+    assert w_obj.argument_at(2) == "c"
+
+def test_compute_new_shape():
+    s = shape.SharingShape("f", [
+        shape.WrapShape(term.Callable.build("a")),
+        shape.InStorageShape(5),
+        shape.InStorageShape(2),
+        shape.InStorageShape(2),
+    ], False)
+    memo = {}
+    ns = s._compute_new_shape(memo)
+    assert s.children[0] is ns.children[0]
+    assert ns.children[1].num == 0
+    assert ns.children[2].num == 1
+    assert ns.children[3].num == 1
+    assert memo == {5:0, 2:1}
+
+def test_make_reshaper():
+    s = shape.SharingShape("f", [
+        shape.WrapShape(term.Callable.build("a")),
+        shape.InStorageShape(5),
+        shape.InStorageShape(2),
+        shape.InStorageShape(2),
+    ], False)
+    rs = shape.make_reshaper(s)
+    ns = rs.newshape
+    assert s.children[0] is ns.children[0]
+    assert ns.children[1].num == 0
+    assert ns.children[2].num == 1
+    assert ns.children[3].num == 1
+    assert rs.storage_shaper == [5, 2]
