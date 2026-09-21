75d6e127a1c37898a924d544903c48067cb73891
CF Bolz-Tereick <cfbolz@gmx.de>
2026-09-21 17:06:49 +0200

    Handle cycles in variable collection and defer output unification

--- prolog/builtin/term_variables.py
+++ prolog/builtin/term_variables.py
@@ -1,7 +1,7 @@
 from rpython.rlib.objectmodel import specialize
 from prolog.builtin.register import expose_builtin
 from prolog.interpreter import term
-from prolog.interpreter.helper import wrap_list, is_term
+from prolog.interpreter.helper import wrap_list, is_term, conssig
 from prolog.interpreter.memo import CopyMemo
 from prolog.interpreter.term import Var, AttVar, Callable
 from prolog.interpreter.error import UnificationFailed
@@ -13,27 +13,41 @@ def impl_term_variables(engine, heap, prolog_term, variables):
 @specialize.arg(4)
 def term_variables(engine, heap, prolog_term, variables, consider_attributes=False):
     varlist = []
-    varc = variables
+    output_cursor = variables
     seen = {}
     todo = [prolog_term]
     cls = Var
     if consider_attributes:
         cls = AttVar
     while todo:
-        t = todo.pop()
-        value = t.dereference(heap)
+        value = todo.pop()
+        if isinstance(value, Var):
+            # Remember variables before following bindings, so back-edges in
+            # cyclic terms are skipped as well as repeated free variables.
+            if value in seen:
+                continue
+            seen[value] = None
+            binding = value.getbinding()
+            if binding is not None:
+                todo.append(binding)
+                continue
         if isinstance(value, cls):
             if consider_attributes and value.is_empty():
                 continue
-            if value not in seen:
-                varlist.append(value)
-                seen[value] = None
-                X = heap.newvar()
-                prolog_list = Callable.build(".", [value, X])
-                prolog_list.unify(varc, heap)
-                varc = X
+            # Check capacity without binding output elements: they may alias
+            # variables in the input. An open tail imposes no further limit.
+            if output_cursor is not None:
+                output_cursor = output_cursor.dereference(heap)
+                if isinstance(output_cursor, Var):
+                    output_cursor = None
+                elif (isinstance(output_cursor, Callable) and
+                      output_cursor.signature().eq(conssig)):
+                    output_cursor = output_cursor.argument_at(1)
+                else:
+                    raise UnificationFailed()
+            varlist.append(value)
         elif isinstance(value, Callable):
             numargs = value.argument_count()
             for i in range(numargs - 1, -1, -1):
                 todo.append(value.argument_at(i))
-    varc.unify(Callable.build("[]"), heap)
+    variables.unify(wrap_list(varlist), heap)
