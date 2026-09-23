38c39914437ff563e66a1408882277072fbe6a19
CF Bolz-Tereick <cfbolz@gmx.de>
2026-09-23 14:09:38 +0200

    Read and write UTF-8 text streams separately from binary bytes

--- prolog/builtin/streams.py
+++ prolog/builtin/streams.py
@@ -10,7 +10,7 @@ from prolog.interpreter import helper
 from prolog.builtin.formatting import TermFormatter
 
 from rpython.rlib.streamio import fdopen_as_stream, open_file_as_stream
-from rpython.rlib import rstring
+from rpython.rlib import rstring, rutf8
 
 rwa = {"read": "r", "write": "w", "append": "a"}
 seek_mode = {"bof": os.SEEK_SET, "current": os.SEEK_CUR, "eof": os.SEEK_END}
@@ -33,6 +33,16 @@ def impl_open_options(engine, heap, srcpath, mode, stream, options):
     if not isinstance(stream, term.Var):
         error.throw_type_error("variable", stream)
     opts = make_option_dict(options)
+    kind = opts.get('type', 'text')
+    if kind not in ('text', 'binary'):
+        error.throw_domain_error('stream_option', term.Callable.build(kind))
+    binary = kind == 'binary'
+    encoding = opts.get('encoding', 'octet' if binary else 'utf8')
+    if encoding != ('octet' if binary else 'utf8'):
+        error.throw_domain_error('encoding', term.Callable.build(encoding))
+    if '\x00' in srcpath:
+        error.throw_domain_error('source_sink', term.Callable.build(srcpath))
+    srcpath = rstring.assert_str0(srcpath)
     mode = rwa.get(mode, None)
     if mode is None:
         error.throw_domain_error("io_mode", term.Callable.build(
@@ -52,10 +62,10 @@ def impl_open_options(engine, heap, srcpath, mode, stream, options):
         try:
             if mode == "r":
                 prolog_stream = PrologInputStream(open_file_as_stream(
-                        srcpath, mode, bufmode))
+                        srcpath, mode, bufmode), binary)
             else:
                 prolog_stream = PrologOutputStream(open_file_as_stream(
-                        srcpath, mode, bufmode))
+                        srcpath, mode, bufmode), binary)
         except OSError:
             error.throw_existence_error("source_sink", term.Callable.build(srcpath))
             assert 0, "unreachable"
@@ -92,33 +102,52 @@ def impl_close(engine, heap, stream):
         except KeyError:
             pass
 
-def read_unicode_char(stream):  
+def check_stream_type(stream, binary, operation):
+    if stream.binary != binary:
+        kind = 'binary_stream' if stream.binary else 'text_stream'
+        error.throw_permission_error(operation, kind, term.Callable.build(stream.alias))
+
+
+def read_unicode_char(stream):
     assert isinstance(stream, PrologInputStream)
+    check_stream_type(stream, False, 'input')
     c = stream.read(1)
-    bytes_read = 1
     if c == "":
         return "end_of_file", 0
-    if ord(c[0]) > 127: # beyond ASCII, so a character consists of 2 bytes
-        c += stream.read(1)
-        bytes_read += 1
-    return c, bytes_read
+    first = ord(c[0])
+    size = 1
+    if 0xc2 <= first <= 0xdf:
+        size = 2
+    elif 0xe0 <= first <= 0xef:
+        size = 3
+    elif 0xf0 <= first <= 0xf4:
+        size = 4
+    elif first >= 0x80:
+        error.throw_representation_error('character')
+    while len(c) < size:
+        byte = stream.read(1)
+        if not byte:
+            error.throw_representation_error('character')
+        if not 0x80 <= ord(byte[0]) <= 0xbf:
+            stream.unread(byte)
+            error.throw_representation_error('character')
+        c += byte
+    try:
+        rutf8.check_utf8(c, allow_surrogates=False)
+    except rutf8.CheckError:
+        error.throw_representation_error('character')
+    return c, size
 
 def peek_unicode_char(stream):
     c, num = read_unicode_char(stream)
     if num > 0:
-        try:
-            stream.seek(-num, os.SEEK_CUR)
-        except OSError:
-            pass
+        stream.unread(c)
     return c
 
 def peek_byte(stream):
     byte = stream.read(1)
     if byte != '':
-        try:
-            stream.seek(-1, os.SEEK_CUR)
-        except OSError:
-            pass
+        stream.unread(byte)
         return ord(byte[0])
     return -1
 
@@ -146,6 +175,7 @@ def impl_get_char_1(engine, heap, obj):
 @expose_builtin("get_byte", unwrap_spec=["instream", "obj"])
 def impl_get_byte(engine, heap, stream, obj):
     assert isinstance(stream, PrologInputStream)
+    check_stream_type(stream, True, 'input')
     byte = stream.read(1)
     if byte != '':
         code = ord(byte[0])
@@ -159,7 +189,9 @@ def impl_get_byte_1(engine, heap, obj):
 
 @expose_builtin("get_code", unwrap_spec=["instream", "obj"])
 def impl_get_code(engine, heap, stream, obj):
-    impl_get_byte(engine, heap, stream, obj)
+    char, size = read_unicode_char(stream)
+    code = rutf8.codepoint_at_pos(char, 0) if size else -1
+    obj.unify(term.Number(code), heap)
 
 @expose_builtin("get_code", unwrap_spec=["obj"])
 def impl_get_code_1(engine, heap, obj):
@@ -178,23 +210,22 @@ def impl_peek_char(engine, heap, stream, obj):
 
 @expose_builtin("peek_byte", unwrap_spec=["instream", "obj"])
 def impl_peek_byte(engine, heap, stream, obj):
+    check_stream_type(stream, True, 'input')
     byte = peek_byte(stream)
     obj.unify(term.Number(byte), heap)
 
 @expose_builtin("peek_code", unwrap_spec=["instream", "obj"])
 def impl_peek_code(engine, heap, stream, obj):
-    impl_peek_byte(engine, heap, stream, obj)
+    char = peek_unicode_char(stream)
+    code = -1 if char == 'end_of_file' else rutf8.codepoint_at_pos(char, 0)
+    obj.unify(term.Number(code), heap)
 
 @expose_builtin("put_char", unwrap_spec=["outstream", "atom"])
 def impl_put_char(engine, heap, stream, atom):
-    length = len(atom)
-    if length == 1:
+    check_stream_type(stream, False, 'output')
+    if rutf8.get_utf8_length(atom) == 1:
         stream.write(atom)
         return
-    elif length == 2:
-        if ord(atom[0]) > 127: # not ASCII
-            stream.write(atom)
-            return
     error.throw_type_error("character", term.Callable.build(atom))
 
 @expose_builtin("put_char", unwrap_spec=["atom"])
@@ -203,7 +234,8 @@ def impl_put_char_1(engine, heap, obj):
 
 @expose_builtin("put_byte", unwrap_spec=["outstream", "int"])
 def impl_put_byte(engine, heap, stream, byte):
-    if byte < 0:
+    check_stream_type(stream, True, 'output')
+    if byte < 0 or byte > 255:
         # XXX have to care about bigints
         error.throw_type_error("byte", term.Number(byte))
     stream.write(chr(byte))
@@ -211,6 +243,32 @@ def impl_put_byte(engine, heap, stream, byte):
 @expose_builtin("put_byte", unwrap_spec=["int"])
 def impl_put_byte_1(engine, heap, obj):
     impl_put_byte(engine, heap, engine.streamwrapper.current_outstream, obj)
+
+
+@expose_builtin('put_code', unwrap_spec=['outstream', 'obj'])
+def impl_put_code(engine, heap, stream, code):
+    check_stream_type(stream, False, 'output')
+    stream.write(helper.unwrap_char_code(code))
+
+
+@expose_builtin('put_code', unwrap_spec=['obj'])
+def impl_put_code_1(engine, heap, code):
+    impl_put_code(engine, heap, engine.streamwrapper.current_outstream, code)
+
+
+@expose_builtin('peek_char', unwrap_spec=['obj'])
+def impl_peek_char_1(engine, heap, obj):
+    impl_peek_char(engine, heap, engine.streamwrapper.current_instream, obj)
+
+
+@expose_builtin('peek_code', unwrap_spec=['obj'])
+def impl_peek_code_1(engine, heap, obj):
+    impl_peek_code(engine, heap, engine.streamwrapper.current_instream, obj)
+
+
+@expose_builtin('peek_byte', unwrap_spec=['obj'])
+def impl_peek_byte_1(engine, heap, obj):
+    impl_peek_byte(engine, heap, engine.streamwrapper.current_instream, obj)
 
 @expose_builtin("current_input", unwrap_spec=["obj"])
 def impl_current_input(engine, heap, obj):
---
+++ prolog/interpreter/test/test_unicode_streams.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+import os
+import pytest
+from rpython.rlib.streamio import fdopen_as_stream
+from prolog.interpreter.stream import PrologInputStream
+from prolog.builtin.streams import read_unicode_char, peek_unicode_char
+from prolog.interpreter.test.tool import assert_true, prolog_raises
+
+
+def test_utf8_file_roundtrip(tmpdir):
+    path = str(tmpdir.join('unicode.txt'))
+    assert_true("open('%s',write,S,[encoding(utf8)]), "
+                "put_char(S,'é'), put_code(S,8364), put_code(S,128512), "
+                "put_code(S,0), close(S)." % path)
+    assert open(path, 'rb').read() == 'é€😀\x00'
+    assert_true("open('%s',read,S), peek_code(S,233), peek_char(S,'é'), "
+                "get_code(S,233), get_char(S,'€'), peek_code(S,128512), "
+                "get_code(S,128512), get_code(S,0), peek_code(S,-1), "
+                "get_char(S,end_of_file), close(S)." % path)
+
+
+def test_utf8_pipe_peeking():
+    readfd, writefd = os.pipe()
+    os.write(writefd, 'é€😀')
+    os.close(writefd)
+    stream = PrologInputStream(fdopen_as_stream(readfd, 'r'))
+    try:
+        for char in ['é', '€', '😀']:
+            assert peek_unicode_char(stream) == char
+            assert peek_unicode_char(stream) == char
+            assert read_unicode_char(stream) == (char, len(char))
+        assert peek_unicode_char(stream) == 'end_of_file'
+    finally:
+        stream.close()
+
+
+@pytest.mark.parametrize('data', ['\xff', '\xc0\x80', '\xed\xa0\x80', '\xf4\x90\x80\x80', '\xe2\x82', '\xe2A'])
+def test_invalid_utf8_stream(tmpdir, data):
+    path = tmpdir.join('bad.txt')
+    path.write(data, mode='wb')
+    prolog_raises('representation_error(character)',
+                  "open('%s',read,S), get_code(S,_)" % path)
+
+
+def test_binary_bytes_and_stream_types(tmpdir):
+    path = str(tmpdir.join('bytes'))
+    assert_true("open('%s',write,S,[type(binary)]), put_byte(S,0), "
+                "put_byte(S,255), close(S), open('%s',read,R,[type(binary)]), "
+                "peek_byte(R,0), get_byte(R,0), get_byte(R,255), "
+                "get_byte(R,-1), close(R)." % (path, path))
+    prolog_raises('permission_error(input,binary_stream,_)',
+                  "open('%s',read,S,[type(binary)]),get_char(S,_)" % path)
+    prolog_raises('permission_error(input,text_stream,_)',
+                  "open('%s',read,S),get_byte(S,_)" % path)
+    prolog_raises('permission_error(output,binary_stream,_)',
+                  "open('%s',write,S,[type(binary)]),put_code(S,233)" % path)
+    prolog_raises('type_error(byte,256)',
+                  "open('%s',write,S,[type(binary)]),put_byte(S,256)" % path)
